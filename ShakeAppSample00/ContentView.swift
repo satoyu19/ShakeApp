549e03067ea7818d1bf0e6185bfb849dce43c529
satoyu19 <96398365+satoyu19@users.noreply.github.com>
2022-07-27 15:53:43 +0900

    Initial Commit

---
+++ ShakeAppSample00/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShakeAppSample00
+//
+//  Created by cmStudent on 2022/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShakeAppSample00/ShakeAppSample00App.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeAppSample00App.swift
+//  ShakeAppSample00
+//
+//  Created by cmStudent on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct ShakeAppSample00App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
